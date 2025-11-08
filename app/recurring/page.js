7c32b1c1099ea6bb73309228f6c0a787f60dfb3e
David Botha <david@botha.dev>
2025-11-09 01:30:42 +0200

    feat: add recurring transaction processing utilities

---
+++ app/recurring/page.js
@@ -0,0 +1,9 @@
+'use client'
+
+import { getRecurringTransactions, saveRecurringTransactions } from "@/lib/recurring"
+
+const RecurringTransactions = () => {
+  return <></>
+}
+
+export default RecurringTransactions
